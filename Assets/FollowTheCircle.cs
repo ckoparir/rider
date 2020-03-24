af67380e2ab2fdab1b866b1b1dd409edcd8f4d31
Caglar KOPARIR <ckoparir@gmail.com>
2020-03-25 00:34:21 +0300

    project created

---
+++ Assets/FollowTheCircle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTheCircle : MonoBehaviour
+{
+	// Start is called before the first frame update
+
+	public Transform circleTransform;
+	private Vector3 _cameraOffset;
+	[Range(0.01f, 1.0f)]
+	public float smoothFactor = 0.5f;
+
+	void Start()
+	{
+		_cameraOffset = transform.position - circleTransform.position;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 newPos = circleTransform.position + _cameraOffset;
+		transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+	}
+}
---
+++ Assets/MoveTheCircle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTheCircle : MonoBehaviour
+{
+	public float jumpHeight;
+	public float moveSpeed;
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+
+		}
+		if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		}
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		}
+	}
+}
